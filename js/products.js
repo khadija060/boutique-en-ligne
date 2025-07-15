a5070afd25cb975989a7d1e7b1b38f2a2743f520
khadija060 <khadijcisse@gmail.com>
2025-07-15 23:49:56 +0000

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,84 @@
+const products = [
+  {
+    id: 1,
+    name: "parfum femme",
+    description: "parfum au senteur sensuel et unique",
+    price: 50000,
+    category: "parfum",
+    image: "images/parfumf.png"
+  },
+  {
+    id: 2,
+    name: "parfum homme",
+    description: "parfum intense et frappant",
+    price: 40000,
+    category: "parfum",
+    image: "images/parfumh.png"
+  },
+  {
+    id: 3,
+    name: "maquillage",
+    description: "soyez specials et sentez vous exceptionnel avec nos maquillages",
+    price: 14000,
+    category: "maquillage",
+    image: "images/makeup.png"
+  },
+  {
+    id: 4,
+    name: "bijoux ",
+    description: "soyez specials et sentez vous exceptionnel avec nos bijoux",
+    price: 10000,
+    category: "bijoux",
+    image: "images/bijoux1.png"
+  },
+   {
+    id: 5,
+    name: "bijoux ",
+    description: "soyez specials et sentez vous exceptionnel avec nos bijoux",
+    price: 35000,
+    category: "bijoux",
+    image: "images/bijoux1.png"
+  },
+   {
+    id: 6,
+    name: "bijoux ",
+    description: "soyez specials et sentez vous exceptionnel avec nos bijoux",
+    price: 12000,
+    category: "bijoux",
+    image: "images/bijoux1.png"
+  },
+  {
+    id: 7,
+    name: "sac ",
+    description: "des merveilles a avoir",
+    price: 25000,
+    category: "sacs",
+    image: "images/sac1.png"
+  },
+  {
+    id: 10,
+    name: "sac ",
+    description: "des merveilles a avoir",
+    price: 16000,
+    category: "sacs",
+    image: "images/sac2.png"
+  },
+ {
+    id: 8,
+    name: "montre",
+    description: "soyez elegants avec nos montres haute game",
+    price: 15000,
+    category: "montre",
+    image: "images/montre2.png"
+  },
+  
+  {
+    id: 9,
+    name: "montre",
+    description: "soyez elegants avec nos montres haute game",
+    price: 20000,
+    category: "montre",
+    image: "images/montre1.png"
+  },
+  
+];
